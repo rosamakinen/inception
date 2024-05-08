5357167c032faa93f1d972d76f2bf551d96b2f40
Rosa-Maria Makinen <rmakinen@c2r2p10.hive.fi>
2024-05-08 13:30:52 +0300

    running nginx only

---
+++ srcs/requirements/mariadb/mariadb_setup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Prepare directories and rights
+mkdir -p /var/lib/mysql /run/mysqld /var/log/mysql
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/log/mysql
+chown -R mysql:mysql /var/lib/mysql
+
+# init database
+mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+
+# Enforce root pw, create db, add user, give rights
+mysqld --user=mysql --bootstrap << EOF
+USE mysql;
+FLUSH PRIVILEGES;
+
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test; 
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+CREATE DATABASE $WORDPRESS_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$WORDPRESS_DB_USER'@'%' IDENTIFIED by '$WORDPRESS_DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'%';
+GRANT ALL PRIVILEGES ON *.* TO '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD' WITH GRANT OPTION;
+GRANT SELECT ON mysql.* TO '$WORDPRESS_DB_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+exec mysqld --defaults-file=/etc/my.cnf.d/mariadb.cnf
