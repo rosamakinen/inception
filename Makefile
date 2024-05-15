12bf8e13ca5b21d0e74b313d4be227004cc38a9e
Rosa-Maria Makinen <rmakinen@c2r6p13.hive.fi>
2024-05-15 07:41:32 +0300

    fixed nginx & mariadb docker files, serving wordpress

--- Makefile
+++ Makefile
@@ -1,4 +1,12 @@
 all:
+	if ! grep -q "rmakinen.42.fr" /etc/hosts; then \
+		echo "127.0.0.1 rmakinen.42.fr" >> /etc/hosts; \
+	fi
+	if ! grep -q "www.rmakinen.42.fr" /etc/hosts; then \
+		echo "127.0.0.1 www.rmakinen.42.fr" >> /etc/hosts; \
+	fi
+	mkdir -p /home/rmakinen/data/mariadb-data
+	mkdir -p /home/rmakinen/data/wordpress-data
 	docker-compose -f srcs/docker-compose.yml build
 	docker-compose -f srcs/docker-compose.yml up -d
 
--- srcs/requirements/mariadb/mariadb_setup.sh
+++ srcs/requirements/mariadb/mariadb_setup.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 # Prepare directories and rights
+echo "this is ok"
 mkdir -p /var/lib/mysql /run/mysqld /var/log/mysql
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/log/mysql
 chown -R mysql:mysql /var/lib/mysql
 
 # init database
+echo "this is ok too"
 mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
-
+echo "is this the prob"
 # Enforce root pw, create db, add user, give rights
 mysqld --user=mysql --bootstrap << EOF
+
 USE mysql;
 FLUSH PRIVILEGES;
 
-DELETE FROM mysql.user WHERE User='';
-DROP DATABASE IF EXISTS test; 
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test ;
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-CREATE DATABASE $WORDPRESS_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS $WORDPRESS_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER '$WORDPRESS_DB_USER'@'%' IDENTIFIED by '$WORDPRESS_DB_PASSWORD';
 GRANT ALL PRIVILEGES ON $WORDPRESS_DB_NAME.* TO '$WORDPRESS_DB_USER'@'%';
 GRANT ALL PRIVILEGES ON *.* TO '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_PASSWORD' WITH GRANT OPTION;
@@ -28,4 +28,4 @@ GRANT SELECT ON mysql.* TO '$WORDPRESS_DB_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-exec mysqld --defaults-file=/etc/my.cnf.d/mariadb.cnf
+exec mysqld --defaults-file=/etc/my.cnf.d/my.cnf
